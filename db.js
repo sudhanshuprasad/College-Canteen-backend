dec70e3d01225efaf9c028b6e4550cb764d3b7cd
sudhanshuprasad <sudhanshuprasad.sp@gmail.com>
2022-04-24 19:35:30 +0530

    random string in mongouri

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // const mongoURI = "mongodb://localhost:27017/College-Canteen?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
 const connectToMongo = ()=>{
-    mongoose.connect(process.env.MONGO_URI, ()=>{
+    mongoose.connect("process.env.MONGO_URI", ()=>{
         console.log("Connected to mongoose");
     })
 }
