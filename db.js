83697e546a2bcb1c0ed211c9df350f156a80c3fa
sudhanshuprasad <sudhanshuprasad.sp@gmail.com>
2022-04-24 19:44:19 +0530

    async mongoose

--- db.js
+++ db.js
@@ -2,8 +2,18 @@ const mongoose = require('mongoose');
 
 // const mongoURI = "mongodb://localhost:27017/College-Canteen?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
+await mongo().then(async _mongoose => {
+    try{
+        console.log('actually Connected to mongo!!');
+        await command.execute(client, message, args);
+    }
+    finally{
+        _mongoose.connection.close();
+    }
+});
+
 const connectToMongo = ()=>{
-    mongoose.connect("process.env.MONGO_URI", ()=>{
+    mongoose.connect(process.env.MONGO_URI, ()=>{
         console.log("Connected to mongoose");
     })
 }
