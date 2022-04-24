514a612dc5cd486bf1178daeef19cb1713433708
sudhanshuprasad <sudhanshuprasad.sp@gmail.com>
2022-04-24 20:05:18 +0530

    clg err

--- db.js
+++ db.js
@@ -2,18 +2,11 @@ const mongoose = require('mongoose');
 
 // const mongoURI = "mongodb://localhost:27017/College-Canteen?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
-await mongo().then(async _mongoose => {
-    try{
-        console.log('actually Connected to mongo!!');
-        await command.execute(client, message, args);
-    }
-    finally{
-        _mongoose.connection.close();
-    }
-});
-
 const connectToMongo = ()=>{
-    mongoose.connect(process.env.MONGO_URI, ()=>{
+    mongoose.connect(process.env.MONGO_URI, (err)=>{
+        if(err){
+            console.log(err);
+        }
         console.log("Connected to mongoose");
     })
 }
