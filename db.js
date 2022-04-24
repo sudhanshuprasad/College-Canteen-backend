823f5aacd6fca9ecf92781a788d46d5c0f1c239e
sudhanshuprasad <sudhanshuprasad.sp@gmail.com>
2022-04-25 00:45:16 +0530

    mongouri

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = process.env.MONGO_URI||"mongodb+srv://sudhanshuprasad:sudhanshuprasad@cluster0.2sxri.mongodb.net/test";
+const mongoURI = process.env.MONGO_URI||"mongodb+srv://sudhanshuprasad:sudhanshuprasad@cluster0.2sxri.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, (err)=>{
